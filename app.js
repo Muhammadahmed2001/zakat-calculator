aace15ad7c4755c6c30970fb25704e4e63a1bbfb
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-04-01 01:24:54 +0500

    Add functionality

---
+++ app.js
@@ -0,0 +1,24 @@
+// // var gold = prompt("Enter your gold")
+// // var silver = prompt("Enter your silver")
+// var goldRate = 207800;
+// var silverRate = 2161.39;
+// var goldCost = +gold * goldRate;
+// var silverCost = +Math.round(silver * silverRate);
+
+
+// var totalAssets = amount + goldCost + silverCost;
+// var zakat = totalAssets / 40;
+
+
+
+
+
+
+
+
+function abc() {
+    var amount = document.getElementById("amount");
+    console.log(amount);
+    
+    
+}
