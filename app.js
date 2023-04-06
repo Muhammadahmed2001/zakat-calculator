8ff0b847d1ea16194419c1b2c23ad37d911c551d
Ahmed karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-04-06 17:06:16 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
 
 
-// var goldRate = 207800;
-// var silverRate = 2161.39;
-// var goldCost = +gold * goldRate;
-// var silverCost = +Math.round(silver * silverRate);
+
 
 
 // var totalAssets = amount + goldCost + silverCost;
+
+
+
+
+
 // var zakat = totalAssets / 40;
 
 
-
-
-
-
-
 function abc() {
+    
 
-    //alert(1);
     var amount = +document.getElementById("cash").value;
     var gold = +document.getElementById("gold").value;
     var silver = +document.getElementById("silver").value;
-    // var total = amount + gold + silver;
-    // var assets = document.getElementById("assets").value;
-    //alert(2);
-    var totalZakat = document.getElementById("assets");
-    totalZakat.value = amount + gold + silver;
-    //alert(totalZakat)
-
-    // console.log(amount.value);
-    // console.log(gold.value);
-    // console.log(silver.value);
-    // console.log(total.value);
-
+    var assets = +document.getElementById("assets").value;
+    
+    var goldRate = 207800;
+    var silverRate = 2161.39;
+    var goldCost = gold * goldRate;
+    var silverCost = Math.round(silver * silverRate);
+    
+    
+    
+    
+    
+    
+    totalZakat = amount + goldCost + silverCost + assets;
     var end_amount = document.getElementById("networth");
-    end_amount.innerHTML = totalZakat.value;
+    end_amount.innerHTML = totalZakat;
+
+
+    var zakatAmount = document.getElementById("zakatamount");
+    zakatAmount.innerHTML = Math.round(totalZakat / 40);
+
+
+
+
+
+
+
+
 
     event.preventDefault();
 }
